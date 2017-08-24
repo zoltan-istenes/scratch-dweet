4cc42f9421248f7225c787cdfb735fea586b0884
zoltan-istenes <zoltan.istenes@gmail.com>
2017-08-25 01:55:15 +0200

    Update dweet.js

--- dweet.js
+++ dweet.js
@@ -21,33 +21,9 @@
         });
     };
 
-       ext.get_dweetA = function(thing, key, callback) {
-        // Make an AJAX call to dweet.io 
-        $.ajax({
-              url: 'https://dweet.io/get/latest/dweet/for/'+thing,
-              dataType: 'json',
-              success: function( get_result ) {
-                  // Got the data - parse it and return the temperature
-                  value = get_result['this'];
-                  callback(value);
-              }
-        });
-    };
     
-      ext.get_dweetB = function(thing, key, callback) {
-        // Make an AJAX call to dweet.io 
-        $.ajax({
-              url: 'https://dweet.io/get/latest/dweet/for/'+thing,
-              dataType: 'json',
-              success: function( get_result ) {
-                  // Got the data - parse it and return the temperature
-                  value = get_result['with'];
-                  callback(JSON.stringify(value));
-              }
-        });
-    };
     
-      ext.get_dweetC = function(thing, key, callback) {
+      ext.get_dweet = function(thing, key, callback) {
         // Make an AJAX call to dweet.io 
         $.ajax({
               url: 'https://dweet.io/get/latest/dweet/for/'+thing,
@@ -60,27 +36,13 @@
         });
     };
     
-      ext.get_dweetD = function(thing, key, callback) {
-        // Make an AJAX call to dweet.io 
-        $.ajax({
-              url: 'https://dweet.io/get/latest/dweet/for/'+thing,
-              dataType: 'json',
-              success: function( get_result ) {
-                  // Got the data - parse it and return the temperature
-                  value = get_result.with[0].content.message;
-                  callback(value);
-              }
-        });
-    };
+     
     
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['R', 'send dweet 16 for thing %s key %s value %s', 'send_dweet', 'scratch_conf_2017', 'message', 'hello' ],
-            ['R', 'get dweet for thing this %s key %s', 'get_dweetA', 'scratch_conf_2017', 'message' ],
-            ['R', 'get dweet for thing JSON stringify with %s key %s', 'get_dweetB', 'scratch_conf_2017', 'message' ],
-            ['R', 'get dweet for thing with 0 1 key %s key %s', 'get_dweetC', 'scratch_conf_2017', 'message' ],
-            ['R', 'get dweet for thing with 0 1 created %s key %s', 'get_dweetD', 'scratch_conf_2017', 'message' ]
+            ['R', 'send dweet for thing %s key %s value %s', 'send_dweet', 'scratch_conf_2017', 'message', 'hello' ],
+            ['R', 'get dweet for thing %s key %s', 'get_dweet', 'scratch_conf_2017', 'message' ]
         ]
     };
 
